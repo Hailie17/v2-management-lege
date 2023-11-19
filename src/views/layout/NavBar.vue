38f7d0e8f81ca5fcd47e4cafcf46ce756b4f3ffa
Hailie17 <192151452@qq.com>
2023-11-20 02:27:05 +0800

    菜单栏渲染数据

--- src/views/layout/NavBar.vue
+++ src/views/layout/NavBar.vue
@@ -5,7 +5,7 @@
       <span v-show="!isCollapse">通用后台管理系统</span>
     </h1>
     <el-menu
-      default-active="2"
+      default-active="1"
       class="el-menu-vertical-demo"
       background-color="#304156"
       text-color="#fff"
@@ -13,40 +13,77 @@
       :unique-opened="true"
       :collapse-transition="false"
       active-text-color="#ffd04b">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航3</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="3-1">选项3</el-menu-item>
-          <el-menu-item index="3-2">选项4</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
+      <div v-for="(item, idx) in menuData" :key="idx">
+        <el-submenu :index="idx" v-if="item.children">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>{{ item.title }}</span>
+          </template>
+          <el-menu-item v-for="(sitem,sidx) in item.children" :key="sidx" :index="sidx">{{ sitem.title }}</el-menu-item>
+        </el-submenu>
+        <el-menu-item :index="idx" v-else>
+          <i class="el-icon-menu"></i>
+          <span slot="title">{{ item.title }}</span>
+        </el-menu-item>
+      </div>
     </el-menu>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+
 export default {
   data () {
     return {
-
+      menuData: [
+        {
+          title: '首页',
+          path: '/'
+        },
+        {
+          title: '客户管理',
+          path: '/customer',
+          children: [
+            {
+              title: '客户档案',
+              path: '/customer/customer'
+            },
+            {
+              title: '拜访记录',
+              path: '/customer/visit'
+            }
+          ]
+        },
+        {
+          title: '修养预约',
+          path: '/business',
+          children: [
+            {
+              title: '预约信息',
+              path: '/business/appointment'
+            },
+            {
+              title: '服务项',
+              path: '/business/service'
+            },
+            {
+              title: '结算单',
+              path: '/business/statement'
+            }
+          ]
+        },
+        {
+          title: '流程管理',
+          path: '/flow',
+          children: [
+            {
+              title: '审核流程定义',
+              path: '/flow/definition'
+            }
+          ]
+        }
+      ]
     }
   },
   computed: {
@@ -58,31 +95,34 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .navbar {
-    width: 220px;
-    height: 100%;
+.navbar {
+  width: 220px;
+  height: 100%;
+  position: relative;
+  background-color: #304156;
+  box-shadow: 5px 0 5px #ddd;
+  transition: all .3s; // 动画
+  &.isColl {
+    width: 64px;
+  }
+
+  .el-menu {
+    border: none;
+  }
+
+  .main-logo {
+    padding: 10px 16px;
+    color: white;
     position: relative;
-    background-color: #304156;
-    box-shadow: 5px 0 5px #ddd;
-    transition: all .3s; // 动画
-    &.isColl {
-      width: 64px;
-    }
-    .el-menu {
-      border: none;
-    }
-    .main-logo {
-      padding: 10px 16px;
-      color: white;
-      position: relative;
-      span {
-        position: absolute;
-        min-width: 160px;
-        font-weight: bold;
-        display: inline-block;
-        top: 14px;
-        left: 50px;
-      }
+
+    span {
+      position: absolute;
+      min-width: 160px;
+      font-weight: bold;
+      display: inline-block;
+      top: 14px;
+      left: 50px;
     }
   }
+}
 </style>
