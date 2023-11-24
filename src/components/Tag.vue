787d089b26ce4912dcabda7ec0f76d01a9105110
Hailie17 <192151452@qq.com>
2023-11-24 22:05:44 +0800

    解决动态标签栏重复问题，find()

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="tag">
-    <el-tag closable size="medium" v-for="item in tags" :key="item.path" :effect="item.title === $route.meta.title[$route.meta.title.length - 1] ? 'dark' : 'plain'">
+  <div class="tags">
+    <el-tag class="tag" closable size="medium" v-for="item in tags" :key="item.path" :effect="item.title === $route.meta.title[$route.meta.title.length - 1] ? 'dark' : 'plain'">
       {{ item.title }}</el-tag>
   </div>
 </template>
@@ -19,11 +19,15 @@ export default {
     $route: {
       immediate: true, // 立即监听
       handler (val, oldVal) {
-        console.log(val)
-        this.tags.push({
-          title: val.meta.title[val.meta.title.length - 1],
-          path: val.path
+        const bool = this.tags.find(item => {
+          return item.path === val.path
         })
+        if (!bool) {
+          this.tags.push({
+            title: val.meta.title[val.meta.title.length - 1],
+            path: val.path
+          })
+        }
       }
     }
   }
@@ -31,8 +35,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.tag {
+.tags {
   padding-top: 4px;
   padding-left: 20px;
+  .tag {
+    margin-right: 5px;
+  }
 }
 </style>
